59d9e887dabf756412465c58afabc39bc55b2701
Grant Gadomski <grant.gadomski@gmail.com>
2024-12-31 18:33:10 +0000

    Correcting FKs from column name change

--- well-better-db/CreateDB.sql
+++ well-better-db/CreateDB.sql
@@ -12,20 +12,20 @@ CREATE TABLE reference_lookup (
   category text,
   ref_value text,
   is_active boolean,
-  created_by bigint references well_better_user(user_id),
+  created_by bigint references well_better_user(id),
   create_date_time timestamp,
-  updated_by bigint references well_better_user(user_id),
+  updated_by bigint references well_better_user(id),
   update_date_time timestamp
 );
 
 CREATE TABLE practice (
   id BIGSERIAL PRIMARY KEY,
   name text,
-  category bigint references reference_lookup(ref_id),
+  category bigint references reference_lookup(id),
   is_active boolean,
-  created_by bigint references well_better_user(user_id),
+  created_by bigint references well_better_user(id),
   create_date_time timestamp,
-  updated_by bigint references well_better_user(user_id),
+  updated_by bigint references well_better_user(id),
   update_date_time timestamp
 );
 
