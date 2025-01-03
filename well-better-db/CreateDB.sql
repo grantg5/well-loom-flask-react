53df0db51f955d14b9159d1e983bda05fce68e10
Grant Gadomski <grant.gadomski@gmail.com>
2025-01-03 19:17:36 +0000

    Added initial inserts on db creation

--- well-better-db/CreateDB.sql
+++ well-better-db/CreateDB.sql
@@ -20,8 +20,23 @@ CREATE TABLE reference_lookup (
 
 CREATE TABLE practice (
   id BIGSERIAL PRIMARY KEY,
-  name text,
+  practice_name text,
   category bigint references reference_lookup(id),
+  source bigint references reference_lookup(id),
+  practice_description text,
+  is_active boolean,
+  created_by bigint references well_better_user(id),
+  create_date_time timestamp,
+  updated_by bigint references well_better_user(id),
+  update_date_time timestamp
+);
+
+CREATE TABLE practice_resource (
+  id BIGSERIAL PRIMARY KEY,
+  practice_id bigint references practice(id),
+  resource_name text,
+  resource_type bigint references reference_lookup(id),
+  resource_description text,
   is_active boolean,
   created_by bigint references well_better_user(id),
   create_date_time timestamp,
@@ -72,4 +87,38 @@ BEGIN
 END;
 $apply_create_date_time_triggers$;
 
--- TODO: insert initial reference lookup items, practices, user
+-- Initial inserts
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_category", "Spiritual", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_category", "Therapeutic", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_category", "Physical", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_source", "Buddhism", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_source", "Jungian Psychology", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_source", "Internal Family Systems", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_source", "Plant Medicine", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_source", "Cognitive Therapy", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_source", "Aerobic Exercise", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_source", "Nutritional Sciences", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_source", "Strength-based Exercises", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("practice_source", "Flexibility", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("resource_type", "Mobile App", t) ON CONFLICT (ref_value) DO NOTHING;
+INSERT INTO reference_lookup (category, ref_value, is_active) VALUES ("resource_type", "Book", t) ON CONFLICT (ref_value) DO NOTHING;
+
+INSERT INTO practice (practice_name, category, source, practice_description, is_active)
+  VALUES ("TM-style Meditation", 1, 4, "Meditating using a mantra method. Popularized in the west by Transcendental Meditation. Vedic meditation, or the use of the free app 1 Giant Mind also fits the bill.", t) ON CONFLICT (practice_name) DO NOTHING;
+INSERT INTO practice (practice_name, category, source, practice_description, is_active)
+  VALUES ("Parts Work", 2, 5, "Unblending from, gaining the trust of protectors, and eventually healing exiles", t) ON CONFLICT (practice_name) DO NOTHING;
+INSERT INTO practice (practice_name, category, source, practice_description, is_active)
+  VALUES ("Therapeutic mushroom trip", 1, 7, "Leveraging a significant psychoblin dose for spiritual discoveries", t) ON CONFLICT (practice_name) DO NOTHING;
+INSERT INTO practice (practice_name, category, source, practice_description, is_active)
+  VALUES ("Therapeutic MDMA trip", 2, 7, "Leveraging MDMA to heal past trauma, wounds, and build love for oneself & others", t) ON CONFLICT (practice_name) DO NOTHING;
+INSERT INTO practice (practice_name, category, source, practice_description, is_active)
+  VALUES ("Running", 3, 9, "Building aerobic & leg muscles through regular running", t) ON CONFLICT (practice_name) DO NOTHING;
+INSERT INTO practice (practice_name, category, source, practice_description, is_active)
+  VALUES ("Climbing", 3, 11, "Building muscles (and having fun) through regular climbing", t) ON CONFLICT (practice_name) DO NOTHING;
+INSERT INTO practice (practice_name, category, source, practice_description, is_active)
+  VALUES ("Increasing plants in diet", 3, 10, "Improving overall sense of physical well-being through more plants (fruits & vegitables) in diet", t) ON CONFLICT (practice_name) DO NOTHING;   
+INSERT INTO practice (practice_name, category, source, practice_description, is_active)
+  VALUES ("Reducing alcohol", 3, 10, "Improving overall sense of physical well-being through reducing drinks per week", t) ON CONFLICT (practice_name) DO NOTHING;
+
+INSERT INTO practice_resource (practice_id, resource_name, resource_type, resource_description, is_active) VALUES (1, "One Giant Mind", 13, "Free app that teaches a mantra-based meditation similar to transcendental meditation", t) ON CONFLICT (resource_name) DO NOTHING;
+INSERT INTO practice_resource (practice_id, resource_name, resource_type, resource_description, is_active) VALUES (1, "Self Therapy", 13, "Book by Jay Earley on the Internal Family Systems method of therapy", t) ON CONFLICT (resource_name) DO NOTHING;
