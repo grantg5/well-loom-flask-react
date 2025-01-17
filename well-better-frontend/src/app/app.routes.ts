8a6d785af900de137b347454afd47893fd6333b6
Grant Gadomski <grant.gadomski@gmail.com>
2025-01-17 21:36:00 +0000

    Init home & about pages, set up routing

--- well-better-frontend/src/app/app.routes.ts
+++ well-better-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from 'core/components/about/about.component';
+import { HomeComponent } from 'core/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    }
+];
--- well-better-frontend/src/app/core/components/header/header.component.ts
+++ well-better-frontend/src/app/core/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, RouterLink, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
