5a341d2dde8edcbb1ec9c357916c4a1015162011
Grant Gadomski <grant.gadomski@gmail.com>
2025-01-17 23:40:24 +0000

    Created about page

--- well-better-frontend/src/app/core/components/about/about.component.ts
+++ well-better-frontend/src/app/core/components/about/about.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatGridListModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
