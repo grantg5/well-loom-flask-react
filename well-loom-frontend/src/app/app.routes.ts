6da5f8190dc301ff65d35830d4d0337b7fab274a
Grant Gadomski <grant.gadomski@gmail.com>
2025-01-18 19:09:46 +0000

    Renamed to Well Loom

---
+++ well-loom-frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { AboutComponent } from 'core/components/about/about.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: AboutComponent
+    }
+];
---
+++ well-loom-services/src/main/java/com/gadomski/well-better/BackendInitializer.java
@@ -0,0 +1,11 @@
+package com.gadomski.well-loom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendInitializer.class, args);
+    }
+}
