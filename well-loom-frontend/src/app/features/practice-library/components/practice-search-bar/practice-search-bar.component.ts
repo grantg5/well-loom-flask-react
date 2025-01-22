001f1a8f260d8a2ca6906ce76834de9ac781e4cf
Grant Gadomski <grant.gadomski@gmail.com>
2025-01-22 01:44:19 +0000

    Created initial practice search sections

--- well-loom-frontend/src/app/features/practice-library/components/practice-search-bar/practice-search-bar.component.ts
+++ well-loom-frontend/src/app/features/practice-library/components/practice-search-bar/practice-search-bar.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatGridList } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-practice-search-bar',
   standalone: true,
-  imports: [MatFormFieldModule, MatGridList, MatSelectModule, MatInputModule, FormsModule],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatIcon],
   templateUrl: './practice-search-bar.component.html',
   styleUrl: './practice-search-bar.component.css'
 })
 export class PracticeSearchBarComponent {
-  
+  areaIdsSelected: number[] = [];
+  practiceGroupIdsSelected: number[] = [];
+  challengeIdsSelected: number[] = [];
+
+  searchPractices(searchInput: string) {
+    console.log("Search clicked");
+  }
+
+  areasFiltered() {
+    console.log(`Areas: ${ this.areaIdsSelected }`);
+  }
+
+  practiceGroupsFiltered() {
+    console.log(`Practice Groups: ${ this.practiceGroupIdsSelected }`);
+  }
+
+  challengesFiltered() {
+    console.log(`Areas: ${ this.challengeIdsSelected }`);
+  }
 }
--- well-loom-frontend/src/app/features/practice-library/pages/practice-search/practice-search.component.ts
+++ well-loom-frontend/src/app/features/practice-library/pages/practice-search/practice-search.component.ts
@@ -10,5 +10,14 @@ import { PracticesGroupedCardsComponent } from 'features/practice-library/compon
   styleUrl: './practice-search.component.css'
 })
 export class PracticeSearchComponent {
+  // NOTE: Will need to update practice group filter on area update (not challenges), to only include ones within the updated dropdown.
+  // Ex. Area updated, practice group dropdown should refresh to include what's in area, removing any selected items that aren't in said area
+  
+  // Will need to update area & practice group dropdown on challenge update
 
+  // Will need to update area on practice group dropdown update
+
+  fetchAreasForFilter() {
+    
+  }
 }
