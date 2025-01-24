6602da798f60b49bf710d5994c4939cff1b7813c
Grant Gadomski <grant.gadomski@gmail.com>
2025-01-24 16:10:13 +0000

    Created practiceGroup, theory, challenge interfaces. Switching gears to Spring Boot backend

---
+++ well-loom-frontend/src/app/shared/services/get-practice-groups.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { PracticeGroup } from 'shared/interfaces/practiceGroup';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetPracticeGroupsService {
+
+  constructor() { }
+}
