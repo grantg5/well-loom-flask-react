1c343f5d493c0242b03eade757c7e4d16ad2da96
Grant Gadomski <grant.gadomski@gmail.com>
2025-03-09 16:33:12 +0000

    Renamed util class for better understandability

--- well-loom-services/src/main/java/com/gadomski/utils/UtilFunctions.java
+++ well-loom-services/src/main/java/com/gadomski/utils/UtilFunctions.java
@@ -2,10 +2,10 @@ package com.gadomski.utils;
 
 import java.util.Map;
 
-public final class EntityMappingUtil {
-    private EntityMappingUtil() {}
+public final class UtilFunctions {
+    private UtilFunctions() {}
 
-    public static Map<String, Object> getEntityMapping(String entity1, String entity2) {
+    public static Map<String, Object> getMappingEntity (String entity1, String entity2) {
         // Note: for class can return MappingClassName.class
         // Query new mapping directory table w/ entities
         // To query based on ids, sort entities coming in alphabetically. Strip all special characters & to-lower. Append id to the end.
