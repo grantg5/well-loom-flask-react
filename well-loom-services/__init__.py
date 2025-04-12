8c386591d14bc2c933e3962161c0a5fc478cc7e1
Grant Gadomski <grant.gadomski@gmail.com>
2025-04-12 14:38:31 +0000

    Finalized initial Flask config

--- well-loom-services/__init__.py
+++ well-loom-services/__init__.py
@@ -3,7 +3,7 @@ from psycopg2 import connect
 from string import Template
 from config import DB_URL, DB_USERNAME, DB_PASS
 
-db_connection = psycopg2.connect(Template('dbname=$name user=$user password=$password').substitute(name=DB_URL, user=DB_USERNAME, password=DB_PASS))
+db_connection = connect(Template('dbname=$name user=$user password=$password').substitute(name=DB_URL, user=DB_USERNAME, password=DB_PASS))
 
 def create_app(config):
     app = Flask(__name__)
