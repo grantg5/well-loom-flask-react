65a93d1785bdb0091c266274f8d565b13c64a619
Grant Gadomski <grant.gadomski@gmail.com>
2025-05-24 12:56:04 +0000

    Got Area entity schema, get all areas working

--- well-loom-services/app.py
+++ well-loom-services/app.py
@@ -1,14 +1,8 @@
 from psycopg import connect
 from flask import Flask
 from flask_smorest import Api
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from model.blueprints import area_blp
 
 import flask_config
-
 db_connection = connect(
     host=flask_config.DB_IP,
     port=flask_config.DB_PORT,
@@ -17,6 +11,7 @@ db_connection = connect(
     password=flask_config.DB_PASS
 )
 
+from model.blueprints import area_blp
 app = Flask(__name__)
 app.config["API_TITLE"] = "Well Loom Services"
 app.config["API_VERSION"] = "v1"
@@ -24,5 +19,6 @@ app.config["OPENAPI_VERSION"] = "3.1.0"
 app.config['OPENAPI_URL_PREFIX'] = '/docs'
 app.config['OPENAPI_SWAGGER_UI_PATH'] = '/ui'
 app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
+
 api = Api(app)
 api.register_blueprint(area_blp)
--- well-loom-services/model/blueprints.py
+++ well-loom-services/model/blueprints.py
@@ -1,14 +1,14 @@
 from app import db_connection
 from flask_smorest import Blueprint
 from flask.views import MethodView
-from schemas import AreaSchema
+from .schemas import AreaSchema
 
 from psycopg.rows import dict_row
 from psycopg import sql
 
 area_blp = Blueprint("areas", "areas", url_prefix="/areas", description="Operations on areas")
 
-@area_blp.route("/")
+@area_blp.route("")
 class AreaList(MethodView):
     @area_blp.response(200, AreaSchema(many=True))
     def get(self):
