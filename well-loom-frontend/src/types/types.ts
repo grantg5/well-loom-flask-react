4cc6930f544dd67d5c5a5a01d8ec45fc198a30e2
Grant Gadomski <grant.gadomski@gmail.com>
2025-05-24 22:36:34 +0000

    Got types generated on frontend off OpenAPI spec

---
+++ well-loom-frontend/src/types/types.ts
@@ -0,0 +1,7 @@
+import { components } from './openapi-gen';
+
+export type Area = components['schemas']['Area'];
+export type Practice = components['schemas']['Practice'];
+export type PracticeGroup = components['schemas']['PracticeGroup'];
+export type Challenge = components['schemas']['Challenge'];
+export type Resource = components['schemas']['Resource'];
