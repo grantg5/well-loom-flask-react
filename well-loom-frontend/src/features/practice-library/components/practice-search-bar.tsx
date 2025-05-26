58892b8bb48205bcf728ca6c64bd8dd8c426fe05
Grant Gadomski <grant.gadomski@gmail.com>
2025-05-26 14:26:04 +0000

    Created skeleton for practice search filtering

--- well-loom-frontend/src/features/practice-library/components/practice-search-bar.tsx
+++ well-loom-frontend/src/features/practice-library/components/practice-search-bar.tsx
@@ -1,14 +1,34 @@
+import { Area, PracticeGroup, Challenge}  from '@types/types'
+
 export default function PracticeSearchBar() {
+    // Default these to inital fetch all call
+    const allAreas: Area[]
+    const allPracticeGroups: PracticeGroup[]
+    const allChallenges: Challenge[]
+
+    const filteredAreas: Area[] = allAreas;
+    const filteredPracticeGroups: PracticeGroup[] = allPracticeGroups;
+    const filteredChallenges: Challenge[] = allChallenges;
+
+    // These'll track ids
+    const selectedAreas: number[]
+    const selectedPracticeGroups: number[]
+    const selectedChallenges: number[]
+
+    let searchText: string = "";
+
+    function updateDropdownValues() {
+    // For each selected in the changed dropdown, get relationship ids, and union results, updating filters w/ ids that come back
+
+    }
+
+    // Put dropdowns here
     return (
         <div>Coming soon</div>
     );
-    
 }
 
 export function Dropdown(options: {id: number, name: string}[], selected: number[]) {
     // TODO: Define dropdown in here
 }
 
-function updateDropdownValues() {
-    
-}
